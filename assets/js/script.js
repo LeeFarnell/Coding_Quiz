5ae93958d6cda54632ddd7b992c4bf2bc7c53b39
Lee Farnell <leefarnell@mac.com>
2021-03-21 15:53:19 +0000

    linked button with script

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const startBtnElement = document.getElementById("start-quiz-btn");
+
+const startGame = () => {
+  console.log("START GAME");
+};
+
+startBtnElement.addEventListener("click", startGame);
