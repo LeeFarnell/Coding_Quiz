83a11eb0fd1f9e85b3fd1dd0cca48a890e0c983b
Lee Farnell <leefarnell@mac.com>
2021-03-21 15:55:33 +0000

    added comments to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,10 @@ const startBtnElement = document.getElementById("start-quiz-btn");
 
 const startGame = () => {
   console.log("START GAME");
+  // remove console log
+  // remove intro-section
+  // start timer
+  // Declare timer value
 };
 
 startBtnElement.addEventListener("click", startGame);
