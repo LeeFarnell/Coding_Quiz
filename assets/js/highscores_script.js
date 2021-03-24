6aa8a84bc71a48730607c698c03005e3415c765a
Lee Farnell <leefarnell@mac.com>
2021-03-24 19:28:45 +0000

    Clear button now clears local storage.

--- assets/js/highscores_script.js
+++ assets/js/highscores_script.js
@@ -1,7 +1,13 @@
 const goBackBtn = document.getElementById("go-back");
+const clearBtn = document.getElementById("clear");
 
 const goBack = () => {
   location.href = "/index.html";
 };
 
+const clear = () => {
+  localStorage.clear();
+};
+
 goBackBtn.addEventListener("click", goBack);
+clearBtn.addEventListener("click", clear);
