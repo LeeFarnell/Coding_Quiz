031fd6aedbbf2180def3cc3097e192ade92ada06
Lee Farnell <leefarnell@mac.com>
2021-03-24 20:20:38 +0000

    local storage pull & high scores render

--- assets/js/highscores_script.js
+++ assets/js/highscores_script.js
@@ -7,7 +7,32 @@ const goBack = () => {
 
 const clear = () => {
   localStorage.clear();
+  onLoad();
+};
+
+const getFromLocalStorage = () => {
+  const highScores = localStorage.getItem("highScores");
+  if (highScores) {
+    return highScores;
+  } else {
+    return [];
+  }
+};
+
+const renderHighScoresTable = (highScores) => {
+  if (highScores.length === 0) {
+    console.log("empty");
+  } else {
+    console.log("create table");
+  }
+};
+
+const onLoad = () => {
+  const highScores = getFromLocalStorage();
+  renderHighScoresTable(highScores);
 };
 
 goBackBtn.addEventListener("click", goBack);
 clearBtn.addEventListener("click", clear);
+
+window.addEventListener("load", onLoad);
