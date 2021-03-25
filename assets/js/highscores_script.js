36fa64be1d91971d125c94dbb5380f6d640621ec
Lee Farnell <leefarnell@mac.com>
2021-03-25 17:58:09 +0000

    results now show on highscores page.

--- assets/js/highscores_script.js
+++ assets/js/highscores_script.js
@@ -1,3 +1,4 @@
+const bodyElement = document.body;
 const goBackBtn = document.getElementById("go-back");
 const clearBtn = document.getElementById("clear");
 
@@ -21,10 +22,39 @@ const getFromLocalStorage = () => {
 
 const renderHighScoresTable = (highScores) => {
   if (highScores.length === 0) {
+    const resultsEmpty = noResults();
+    bodyElement.appendChild(resultsEmpty);
     console.log("empty");
   } else {
+    const showResults = resultsTable();
+    bodyElement.appendChild(showResults);
     console.log("create table");
   }
+};
+
+const noResults = () => {
+  const noResultsDiv = document.createElement("div");
+  noResultsDiv.setAttribute("id", "results-div");
+
+  const noResultsH2 = document.createElement("h2");
+  noResultsH2.textContent = "No results to show.";
+
+  noResultsDiv.appendChild(noResultsH2);
+
+  return noResultsDiv;
+};
+
+const resultsTable = () => {
+  const tableDiv = document.createElement("div");
+  tableDiv.setAttribute("id", "table");
+
+  const resultDiv = document.createElement("div");
+  resultDiv.setAttribute("id", "results");
+  resultDiv.textContent = getFromLocalStorage();
+
+  tableDiv.appendChild(resultDiv);
+
+  return tableDiv;
 };
 
 const onLoad = () => {
